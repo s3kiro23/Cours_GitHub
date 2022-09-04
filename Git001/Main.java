6409b037457fff0258beb03da33165674c2970b2
Jm <spawn2x@protonmail.com>
2022-09-04 14:49:01 +0200

    update git002

--- Git001/Main.java
+++ Git001/Main.java
@@ -1,6 +1,7 @@
 public class Main{
     public static void main (String args []){
         System.out.println("Hello from Main");
+        System.out.println("Hello from Main de git002");
         Pangolin p = new Pangolin("tata", 5);
         p.crier();
     }
